e4fc82bfae97238420e1c59b846fd318ce743620
kielek7274 <damian.mlodzik23@gmail.com>
2023-11-28 19:58:32 +0100

    modified:   css/style.css 	new file:   img/10sphynx.jpg 	new file:   img/11siberian.jpg 	new file:   img/12americanshorthair.jpg new file:   img/13bengal.jpg 	new file:   img/14russianblue.jpg 	new file:   img/15norwegianforest.jpg 	new file:   img/16orientalshorthair.jpg 	new file:   img/17siamese.jpg 	new file:   img/18cornishrex.jpg 	new file:   img/19selkirkrex.jpg 	new file:   img/1ragdoll.jpg 	new file:   img/20birman.jpg 	new file:   img/2mainecoon.jpg 	new file:   img/3devonrex.jpg 	new file:   img/4exotic.jpg 	modified:   img/5persian.jpg 	new file:   img/6britishshorthair.JPG 	new file:   img/7abyssinian.jpg 	new file:   img/8domestic.jpg 	new file:   img/9scottishfold.jpg 	modified:   index.html 	modified:   js/example.js

---
+++ b
@@ -0,0 +1,25 @@
+<br />
+<div id="">
+   <table id="">
+       <tr>
+           <td>Weight/Size</td>
+           <td>7-14 Pounds</td>
+       </tr>
+       <tr>
+           <td>Height</td>
+           <td>8-10 Inches</td>
+       </tr>
+       <tr>
+           <td>Life Expentancy</td>
+           <td>15+ years</td>
+       </tr>
+       <tr>
+           <td>Coat and color</td>
+           <td>Hairless breed that comes in a variety of colors and patterns</td>
+       </tr>
+       <tr>
+           <td>Characteristics</td>
+           <td>Large ears, hairless body, outgoing personality</td>
+       </tr>
+   </table></div>
+</div>
--- js/example.js
+++ js/example.js
@@ -1,9 +1,33 @@
 function toggleMenu(element) {
-    const allMenus = document.querySelectorAll('.menu-container');
-    allMenus.forEach(menu => {
+    let allMenus = document.querySelectorAll('.menu-container'); // selects all elements with mentioned class https://www.w3schools.com/jsref/met_document_queryselectorall.asp
+    allMenus.forEach(menu => { // calls a function for each element in array. In here, menu https://www.w3schools.com/jsref/jsref_foreach.asp
         if (menu !== element) {
-            menu.classList.remove("active");
+            menu.classList.remove("active"); // returns classes as an array https://www.w3schools.com/jsref/prop_element_classlist.asp
         }
     });
     element.classList.toggle("active");
-}
+};
+
+document.addEventListener('click', function(event) { // adds a click event https://www.w3schools.com/jsref/met_document_addeventlistener.asp
+    let trigger = event.target.closest('.popup-trigger'); // https://developer.mozilla.org/en-US/docs/Web/API/Element/closest
+    let popups = document.querySelectorAll('.popup');
+
+    popups.forEach(function(popup) {
+        popup.style.display = 'none';
+    });
+
+    if (trigger) {
+        let popupId = trigger.getAttribute('data-popup-id'); // get value of class as element https://www.w3schools.com/jsref/met_element_getattribute.asp
+        let popup = document.getElementById(popupId); // gets the element with mentioned id https://www.w3schools.com/jsref/met_document_getelementbyid.asp
+
+        if (popup) {
+            popup.style.display = 'block'; // display style https://www.w3schools.com/jsref/prop_style_display.asp
+        }
+    }
+});
+
+
+
+
+
+
